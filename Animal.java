b6ee890c5234f0c2d8b8314fa25b9b37a50a27e2
Kirupanandhan <94386222+Kirupanandhan@users.noreply.github.com>
2023-03-19 16:13:41 +0530

    Add files via upload
    Question 1

---
+++ Animal.java
@@ -0,0 +1,9 @@
+package WA2;
+
+public class Animal
+{
+    void walk(){
+        System.out.println("I am walking");
+
+    }
+}
---
+++ Bird.java
@@ -0,0 +1,12 @@
+package WA2;
+
+public class Bird extends Animal
+{
+    void fly() {
+        System.out.print("I am flying");
+    }
+    void sing()
+    {
+        System.out.println(" I am singing");
+    }
+}
